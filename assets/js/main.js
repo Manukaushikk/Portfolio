0f9f55cd03572c8615bc405675b348f51a2549d1
Manu Kaushik <codebymanuk@gmail.com>
2022-11-11 23:57:55 +0530

    upto js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+/*==== MENU SHOW ==== */
+// Validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*==== MENU SHOW ==== */
+// Validate if constant exists
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
@@ -16,10 +34,10 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
