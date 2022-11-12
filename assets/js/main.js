c087425355bb97b315303b3f41de4e5bf0d7661b
Manu Kaushik <codebymanuk@gmail.com>
2022-11-12 14:34:49 +0530

    Email ✅

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,8 +56,44 @@ let swiperProjects = new Swiper(".projects__container", {
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactProject = document.getElementById('contact-project'),
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
 
+    // Check if the field has a value
+    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+        // Add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+
+        // Show Message
+        contactMessage.textContent = 'Write all input fields 📩'
+    } else {
+        emailjs.sendForm('service_4adu78s', 'template_g0cxand', '#contact-form', 'BID6w4JrR421vy2cr')
+            .then(() => {
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Message Sent ✔️'
+
+                // Remove message from five seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 5000);
+            }, (error) => {
+                alert('OOPS! SOMETHING HAS FAILED....🤔', error)
+            })
+        // To clear the input field
+        contactName.value = ''
+        contactEmail.value = ''
+        contactProject.value = ''
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
